ac9632aa4df2e414f59f79fca572976fd3376275
Youssef Nafaa <nafaayoussef@gmail.com>
2022-02-28 11:34:07 -0500

    add header and footer

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="footer" >
+    <p class="footText">&copy; LightHouse Labs 2021-2022. All Rights Reserved.</p>
+</div>
---
+++ header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="https://www.lighthouselabs.ca/">
+				<img src="Images/logo.png" alt="Lighthouse Labs Logo"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="MyFriends.php">My Friends</a></li>
+      <li><a href="MyAlbums.php">My Albums</a></li>
+
+  	  <li><a href="MyPictures.php?whereToGo='My'">My Pictures</a></li>
+	    <li><a href="UploadPictures.php">Upload Pictures</a></li>
+    </ul>
+  </div>
+</nav>
+
