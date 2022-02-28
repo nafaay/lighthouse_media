ac1570893d9c13fb4d5601a9b59c44c5c68c1132
Youssef Nafaa <nafaayoussef@gmail.com>
2022-02-28 11:29:03 -0500

    add main page with style

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="styles/style.css">
+	<title>LightHouse Labs Social Media Website</title>
+  </head>
+    <body>  
+  	<div class="row">
+			<div class="col-md-12" id="welcome">
+			<h3>Welcome to Lighthouse Labs Social Media Website</h3>
+	
+				<p class="lead">If you have never used this before, you have to <a href="NewUser.php">Sign Up </a>first.</p>
+				<p class="lead">If you have already Signed Up, you can <a href="Login.php">Log In </a>now.</p>
+				
+			</div>
+		</div>
+  </body>
+</html>
