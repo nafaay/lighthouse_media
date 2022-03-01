cba6141fbc75faed813c8359bb16028cc5477e1e
Youssef Nafaa <nafaayoussef@gmail.com>
2022-02-28 19:14:57 -0500

    hide sensitive data

--- connexion.php
+++ connexion.php
@@ -1,5 +1,11 @@
 <?php
-	$connBD = new PDO('mysql:host=localhost; dbname=db_lighthouse_media', 'root', ''
+	require_once('hide_connect.php');
+  $port = $_SESSION['port'];
+  $database_name = $_SESSION['database_name'];
+  $user_name = $_SESSION['user_name'];
+  $password  =   $_SESSION['password'];
+
+	$connBD = new PDO("mysql:host=$port; dbname=$database_name", $user_name, $password
 				,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 				,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 			
